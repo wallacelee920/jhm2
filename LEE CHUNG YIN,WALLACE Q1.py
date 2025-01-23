b16fa134622445c5f76f0face6d594f11713944b
wallace <s20017@ypicallmc.edu.hk>
2025-01-23 11:43:06 +0000

    updata

--- LEE CHUNG YIN,WALLACE Q1.py
+++ LEE CHUNG YIN,WALLACE Q1.py
@@ -59,19 +59,3 @@ average_height = sum(heights) / len(heights)
 max_height = max(heights)
 print(f"The average height of these students is {average_height:.2f} cm.")
 print(f"The maximum height of these students is {max_height:.2f} cm.")
-
-# 5.數據可視化
-import pandas as pd
-import matplotlib.pyplot as plt
-data = {
-    'Month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    'Sales': [380, 400, 660, 800, 900, 1200, 1600, 2200, 1500, 1100, 600, 250]
-}
-df = pd.DataFrame(data)
-
-plt.figure(figsize=(10, 6))
-plt.bar(df['Month'], df['Sales'], color='green', width=0.5)
-plt.title('Bar Chart of Ice-cream Sales')
-plt.xlabel('Month')
-plt.ylabel('Sales (in thousand dollars)')
-plt.show()
