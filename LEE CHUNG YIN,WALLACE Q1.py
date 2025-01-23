e9ea632fe97f9050e9ccb6cebccdaff523e4007d
wallace <s20017@ypicallmc.edu.hk>
2025-01-23 11:41:44 +0000

    update

--- LEE CHUNG YIN,WALLACE Q1.py
+++ LEE CHUNG YIN,WALLACE Q1.py
@@ -74,4 +74,4 @@ plt.bar(df['Month'], df['Sales'], color='green', width=0.5)
 plt.title('Bar Chart of Ice-cream Sales')
 plt.xlabel('Month')
 plt.ylabel('Sales (in thousand dollars)')
-plt.show()44
+plt.show()
